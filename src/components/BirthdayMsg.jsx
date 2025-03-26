9f3035215d875918dca13cdbb1cfba63a1514afc
zpikachu <muzaif635@gmail.com>
2025-03-26 23:12:08 +0530

    made changes on Cake Component

--- src/components/BirthdayMsg.jsx
+++ src/components/BirthdayMsg.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import "./birthdayMsg.css"
 function BirthdayMsg() {
+  const searchParams=new URLSearchParams(window.location.search);
+  //2024-21
+  const name=searchParams.get("m") || "Asmitha";
   return (
     <div style={{justifyContent:"center",alignItems:"center",display:"flex"}}>
-      <h1 className='heading'>HAPPY BIRTHDAY Asmitha</h1>
+      <h1 className='heading'>HAPPY BIRTHDAY {name}</h1>
     </div>
   )
 }
--- src/components/Cake.jsx
+++ src/components/Cake.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './cake.css'
 function Cake() {
+  const searchParams=new URLSearchParams(window.location.search);
+  const birthDate=new Date(2003,9,13);
+  const Asmitha_age=parseInt((Date.now()-birthDate.getTime())/31557600000);
+  const age=parseInt(searchParams.get("a")) || Asmitha_age;
   return (
     <div style={{justifyContent:"center",alignItems:"center",display:"flex"}}>
       <div className="box-canvas">
@@ -17,7 +21,7 @@ function Cake() {
     <div className="drip"></div>
 </div>
 <div className="cake-top">
-    <div className="number-container">21</div>
+    <div className="number-container">{age}</div>
 </div>
 </div>
 </div>
